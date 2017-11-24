7064ae0aac710ed48c671d1915de48fb1f33e49e
tinyherocarrot <andrw.huang@gmail.com>
2017-11-24 15:27:29 -0600

    first react page and route

---
+++ client/src/pages/SignIn.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class SignIn extends Component {
+  state = {
+    email: "",
+    password: ""
+  };
+
+  render() {
+    return <h1>[chews]</h1>;
+  }
+}
+
+export default SignIn;
