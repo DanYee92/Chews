78f5b8fc6ba93485f78e3787cefc7c7305620eb0
Andrew Huang <andrw.huang@gmail.com>
2017-11-26 16:07:37 -0600

    WIP

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,5 +1,13 @@
 import React from "react";
-import { Navbar, Nav, NavItem, MenuItem, NavDropdown, FormGroup, FormControl } from "react-bootstrap";
+import {
+  Navbar,
+  Nav,
+  NavItem,
+  MenuItem,
+  NavDropdown,
+  FormGroup,
+  FormControl
+} from "react-bootstrap";
 import { LinkedLogo } from "./Logo";
 import Button from "./Button";
 import styled from "styled-components";
@@ -12,11 +20,29 @@ const MyNav = styled(Navbar)`
 const MyBrand = styled(Navbar.Brand)`
   color: tomato !important;
 
-  @media (max-width: 600px){
-    margin: auto !important;
-    float: none !important;
+  @media (max-width: 768px) {
+    position: absolute;
+    left: 50%;
+    margin-left: -2.5em !important; /* 50% of logo width */
+    display: block;
+  }
+`;
+
+const MyNavToggle = styled(Navbar.Toggle)`
+  border: 0 !important;
+  
+  @media (max-width: 768px) {
+    position: absolute;
+    left: 1em;
+    display: block;
   }
 `
+
+const MyNavCollapse = styled(Navbar.Collapse)`
+  @media (max-width: 768px) {
+    margin-top: 3.5em;
+  }
+`;
 
 class NavBar extends React.Component {
   constructor(props) {
@@ -36,60 +62,62 @@ class NavBar extends React.Component {
 
   updateWindowDimensions() {
     this.setState({
-      width: Math.floor(window.innerWidth * 0.8),
+      width: window.innerWidth,
       height: window.innerHeight
     });
   }
 
   render() {
-    return (
+    return this.state.width > 768 ? (
       <MyNav collapseOnSelect>
         <Navbar.Header>
-          <Navbar.Toggle />
+          <MyNavToggle />
+          <MyBrand>
+            {/* Logo links to Home */}
+            <LinkedLogo href="#" />
+          </MyBrand>
+
+          <Navbar.Form pullLeft>
+            <FormGroup>
+              <FormControl type="text" placeholder="Search" />
+            </FormGroup>{" "}
+            <Button type="submit" primary>
+              Submit
+            </Button>
+          </Navbar.Form>
+        </Navbar.Header>
+
+        <MyNavCollapse>
+          <Nav pullRight>
+            <NavItem eventKey={1} href="#">
+              Sign Up
+            </NavItem>
+            <NavItem eventKey={2} href="#">
+              Log In
+            </NavItem>
+          </Nav>
+        </MyNavCollapse>
+      </MyNav>
+    ) : (
+      <MyNav collapseOnSelect>
+        <MyNavToggle />
+        <Navbar.Header>
           <MyBrand>
             {/* Logo links to Home */}
             <LinkedLogo href="#" />
           </MyBrand>
         </Navbar.Header>
 
-        {this.state.width > 613 ? (
-          <div>
-            <Navbar.Form pullLeft>
-              <FormGroup>
-                <FormControl type="text" placeholder="Search" />
-              </FormGroup>{" "}
-              <Button type="submit" primary>
-                Submit
-              </Button>
-            </Navbar.Form>
-
-            <Navbar.Collapse>
-              <Nav pullRight>
-                <NavItem eventKey={1} href="#">
-                  Sign Up
-                </NavItem>
-                <NavItem eventKey={2} href="#">
-                  Log In
-                </NavItem>
-              </Nav>
-            </Navbar.Collapse>
-          </div>
-        ) : (
-          <div>
-            <p> TEST </p>
-
-            <Navbar.Collapse>
-              <Nav pullLeft>
-                <NavItem eventKey={1} href="#">
-                  Sign Up
-                </NavItem>
-                <NavItem eventKey={2} href="#">
-                  Log In
-                </NavItem>
-              </Nav>
-            </Navbar.Collapse>
-          </div>
-        )}
+        <MyNavCollapse>
+          <Nav pullLeft>
+            <NavItem eventKey={1} href="#">
+              Sign Up
+            </NavItem>
+            <NavItem eventKey={2} href="#">
+              Log In
+            </NavItem>
+          </Nav>
+        </MyNavCollapse>
       </MyNav>
     );
   }
