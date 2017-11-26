d009647a0825bd599f66008de76957a91ee0c988
Nicole Ersing <nicolegiacci@gmail.com>
2017-11-26 14:05:21 -0600

    routes for bites created and working

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -31,15 +31,31 @@ module.exports = app => {
 	});
 
 	//POST - make a new bite
-	// "/api/bite/create"
-	//create a new bite
-
+	app.post("/api/bite/create" , (req, res) => {
+		console.log("new bite created")
+		console.log(req.body)
+		db.Bite.create(req.body).then(newBite => 
+		res.json(newBite))
+	});
+	
 	//GET - get bites from the db
-	// "/api/bites/search"
-	//find all bites where: not booked, and within specified date range
+	//Need to add within a specified date range 
+	app.get("/api/bites/search", (req, res) => {
+		console.log("all available bites to book!")
+		db.Bite
+		//find all bites where: not booked
+		//Need to add within a specified date range
+		.find({isBooked: false})
+		.then(foundBites => res.json(foundBites))
+
+	})
 
 	//PUT - update a bite to be "booked"
-	// "/api/bites/:biteId/book"
-	//find the biteId
-	//and set isbooked = true
+	app.patch("/api/bites/:biteId/book", (req, res) => {
+		console.log("biteId to find:")
+		db.Bite
+		//find the biteId and update isBooked: true
+		.findOneAndUpdate( { _biteId: req.params.id} ,{isBooked: true})
+		.then(biteIsBooked => res.json(biteIsBooked))
+	})
 };
