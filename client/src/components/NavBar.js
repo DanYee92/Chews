a17d01fdad718f4657bf4e8d129749576bb67968
unknown <aliarfeen911@gmail.com>
2017-11-26 15:43:51 -0600

    navbar wip

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,5 +1,13 @@
 import React from "react";
-import { Navbar, Nav, NavItem, MenuItem, NavDropdown, FormGroup, FormControl } from "react-bootstrap";
+import {
+  Navbar,
+  Nav,
+  NavItem,
+  MenuItem,
+  NavDropdown,
+  FormGroup,
+  FormControl
+} from "react-bootstrap";
 import { LinkedLogo } from "./Logo";
 import Button from "./Button";
 import styled from "styled-components";
@@ -8,48 +16,101 @@ const MyNav = styled(Navbar)`
   background: transparent;
   border: none;
 `;
- 
+
+const MyBrand = styled(Navbar.Brand)`
+  color: tomato !important;
+
+  @media (max-width: 768px) {
+    position: absolute;
+    left: 50%;
+    margin-left: -50px !important; /* 50% of your logo width */
+    display: block;
+  }
+`;
+
 const styles = {
   brand: {
     color: "tomato"
   }
-}
+};
 
-const NavBar = () => (
-  <MyNav collapseOnSelect>
-    <Navbar.Header>
-      <Navbar.Brand styles={styles.brand}>
-        {/* Logo links to Home */}
-        <LinkedLogo activehref="#" />
-      </Navbar.Brand>
-      <Navbar.Toggle />
-    </Navbar.Header>
-    <Navbar.Collapse>
-      <Nav>
-        <Navbar.Form pullLeft>
-          <FormGroup>
-            <FormControl type="text" placeholder="Search" />
-          </FormGroup>{" "}
-          <Button type="submit" primary>Submit</Button>
-        </Navbar.Form>
-        <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-          <MenuItem eventKey={3.1}>Action</MenuItem>
-          <MenuItem eventKey={3.2}>Another action</MenuItem>
-          <MenuItem eventKey={3.3}>Something else here</MenuItem>
-          <MenuItem divider />
-          <MenuItem eventKey={3.3}>Separated link</MenuItem>
-        </NavDropdown>
-      </Nav>
-      <Nav pullRight>
-        <NavItem eventKey={1} href="#">
-          Link Right
-        </NavItem>
-        <NavItem eventKey={2} href="#">
-          Link Right
-        </NavItem>
-      </Nav>
-    </Navbar.Collapse>
-  </MyNav>
-);
+class NavBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { width: "0", height: "0" };
+    this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+  }
+
+  componentDidMount() {
+    this.updateWindowDimensions();
+    window.addEventListener("resize", this.updateWindowDimensions);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.updateWindowDimensions);
+  }
+
+  updateWindowDimensions() {
+    this.setState({
+      width: window.innerWidth,
+      height: window.innerHeight
+    });
+  }
+
+  render() {
+    return this.state.width > 768 ? (
+      <MyNav collapseOnSelect>
+        <Navbar.Header>
+          <Navbar.Toggle />
+          <MyBrand>
+            {/* Logo links to Home */}
+            <LinkedLogo href="#" />
+          </MyBrand>
+
+          <Navbar.Form pullLeft>
+            <FormGroup>
+              <FormControl type="text" placeholder="Search" />
+            </FormGroup>{" "}
+            <Button type="submit" primary>
+              Submit
+            </Button>
+          </Navbar.Form>
+        </Navbar.Header>
+
+        <Navbar.Collapse>
+          <Nav pullRight>
+            <NavItem eventKey={1} href="#">
+              Sign Up
+            </NavItem>
+            <NavItem eventKey={2} href="#">
+              Log In
+            </NavItem>
+          </Nav>
+        </Navbar.Collapse>
+      </MyNav>
+    ) : (
+      <MyNav collapseOnSelect>
+        <Navbar.Header>
+          <Navbar.Toggle />
+          <MyBrand>
+            {/* Logo links to Home */}
+            <LinkedLogo href="#" />
+          </MyBrand>
+        </Navbar.Header>
+
+        <Navbar.Collapse>
+          <Nav pullLeft>
+            <NavItem eventKey={1} href="#">
+              Sign Up
+            </NavItem>
+            <NavItem eventKey={2} href="#">
+              Log In
+            </NavItem>
+          </Nav>
+        </Navbar.Collapse>
+      </MyNav>
+    );
+  }
+}
 
 export default NavBar;
