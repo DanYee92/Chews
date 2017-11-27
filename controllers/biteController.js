b7e337c4e51761b137942f8e5b7d30a8d2ffd12b
itkazmi814 <itkazmi814@gmail.com>
2017-11-27 11:55:01 -0600

    moved back end routes into controllers. tested and working

--- controllers/biteController.js
+++ controllers/biteController.js
@@ -1,2 +1,58 @@
 const router = require("express").Router();
 const db = require("../models");
+
+module.exports = {
+	createNewBite: (req, res) => {
+		console.log("new bite created");
+		console.log("User ID making the bite:", req.params.localId);
+		console.log("req.body:", req.body);
+		db.Bite
+			.create(req.body)
+			.then(newBite => {
+				//find the user creating a new bite
+				return db.User.findOneAndUpdate(
+					{ _id: req.params.localId },
+					//add the new bite to the user's array of bites
+					{ $push: { bites: newBite._id } },
+					{ new: true }
+				);
+			})
+			//return the new user, with the newBite in their array
+			.then(userWithNewBite => res.json(userWithNewBite));
+	},
+
+	searchForBites: (req, res) => {
+		console.log("all available bites to book!");
+		console.log("searching for bites in city:", req.params.city);
+		const query = {
+			isBooked: false,
+			city: req.params.city
+		};
+
+		db.Bite
+			//find all bites where: not booked
+			//Need to add within a specified date range
+			.find(query)
+			.then(foundBites => res.json(foundBites));
+	},
+
+	bookBite: (req, res) => {
+		console.log("biteId to find:", req.params.biteId);
+		console.log("UserID booking:", req.params.travelerId);
+		db.Bite
+			//find the biteId and update isBooked: true, and add a travelerId to the bite
+			.findOneAndUpdate(
+				{ _id: req.params.biteId },
+				{ isBooked: true, $set: { travelerId: req.params.travelerId } }
+			)
+			.then(bookedBite => {
+				//add the bite to the traveler's bites
+				return db.User.findOneAndUpdate(
+					{ _id: req.params.travelerId },
+					{ $push: { bites: bookedBite._id } },
+					{ new: true }
+				);
+			})
+			.then(result => res.json(result));
+	}
+};
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,13 @@
 const router = require("express").Router();
 const db = require("../models");
 
-module.exports = () => {
+module.exports = {
+	createNewUser: (req, res) => {
+		console.log("User to be made:");
+		console.log(req.body);
+		db.User.create(req.body).then(newUser => res.json(newUser));
+	},
+
 	getAllUserInfo: (req, res) => {
 		console.log("User ID to find:", req.params.userId);
 		db.User
@@ -10,5 +16,5 @@ module.exports = () => {
 			//populate that user with their bites
 			.populate("bites")
 			.then(foundUser => res.json(foundUser));
-	};
+	}
 };
