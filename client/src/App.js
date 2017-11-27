b6a65f64a296ba049e173cc3ffaabd6c9369f354
unknown <aliarfeen911@gmail.com>
2017-11-27 13:34:51 -0600

    navbar added to all pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import Navbar from "./components/NavBar";
 import SignIn from "./pages/SignIn";
 import Landing from "./pages/Landing";
 import Browse from "./pages/Browse";
@@ -9,6 +10,7 @@ const App = () => {
 	return (
 		<Router>
 			<div>
+				<Navbar />
 				<Route exact path="/" component={Landing} />
 				<Route path="/landing" component={Landing} />
 				<Route path="/signIn" component={SignIn} />
