f08739343c415417d52146086dbea6697b423933
Andrew Huang <andrw.huang@gmail.com>
2017-11-27 23:38:04 -0600

    created basic createListing page

--- client/src/App.js
+++ client/src/App.js
@@ -4,21 +4,20 @@ import Navbar from "./components/NavBar";
 import LogIn from "./pages/LogIn";
 import Landing from "./pages/Landing";
 import Browse from "./pages/Browse";
+import CreateListing from "./pages/CreateListing";
 
 //if not logged in, route to pages/LogIn
 const App = () => {
-	return (
-		<Router>
-			<div>
-				<Navbar />
-				<Route exact path="/" component={Landing} />
-				<Route exact path="/home" component={Landing} />
-				<Route exact path="/login" component={LogIn} />
-				<Route exact path="/browse" component={Browse} />
-				<Route />
-			</div>
-		</Router>
-	);
+	return <Router>
+      <div>
+        <Navbar />
+        <Route exact path="/" component={Landing} />
+        <Route exact path="/home" component={Landing} />
+        <Route exact path="/login" component={LogIn} />
+        <Route exact path="/browse" component={Browse} />
+        <Route exact path="/create" component={CreateListing} />
+      </div>
+    </Router>;
 };
 
 export default App;
