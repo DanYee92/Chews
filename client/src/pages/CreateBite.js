647e86f596bdd157904d73d5ca6a98265365b6dd
unknown <aliarfeen911@gmail.com>
2017-11-28 14:02:04 -0600

    merge conflict cleared

--- client/src/pages/CreateBite.js
+++ client/src/pages/CreateBite.js
@@ -14,7 +14,7 @@ class CreateListing extends Component {
 	};
 
 	handleFormSubmit = event => {
-		// // for dummy data
+		// // for dummy user data
 		// const ali = {
 		// 	firstName: "Ali",
 		// 	lastName: "Arfeen",
@@ -95,48 +95,6 @@ class CreateListing extends Component {
 			</Container>
 		);
 	}
-	// =======
-	//   state = { location: "", startDate: "", endDate: "" };
-
-	//   handleInputChange = event => {
-	//     const { name, value } = event.target;
-	//     this.setState({ [name]: value });
-	//   };
-	//   render() {
-	//     return (
-	//       <Container column>
-	//         <form>
-	//           <FormGroup controlId="createListingForm">
-	//             <ControlLabel>Where do you want to grab a Bite?</ControlLabel>
-	//             <FormInput
-	//               type="text"
-	//               name="email"
-	//               placeholder=""
-	//               onChange={this.handleInputChange}
-	//             />
-	//             <ControlLabel>
-	//               When do you want to grab a Bite? (start)
-	//             </ControlLabel>
-	//             <FormInput
-	//               type="text"
-	//               name="password"
-	//               placeholder=""
-	//               onChange={this.handleInputChange}
-	//             />
-	//             <ControlLabel>When do you want to grab a Bite? (end)</ControlLabel>
-	//             <FormInput
-	//               type="text"
-	//               name="password"
-	//               placeholder=""
-	//               onChange={this.handleInputChange}
-	//             />
-	//             <Button primary> Create Listing </Button>
-	//           </FormGroup>
-	//         </form>
-	//       </Container>
-	//     );
-	//   }
-	// >>>>>>> 95228cef3a841bc0ce09b44a670f0dcb77f18d4a
 }
 
 export default CreateListing;
