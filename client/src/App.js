d9dc243e9645f730c918f2253095c685e18058e0
Nicole Ersing <nicolegiacci@gmail.com>
2017-11-28 14:19:40 -0600

    seeds User

--- client/src/App.js
+++ client/src/App.js
@@ -4,20 +4,29 @@ import Navbar from "./components/NavBar";
 import LogIn from "./pages/LogIn";
 import Landing from "./pages/Landing";
 import Browse from "./pages/Browse";
-import CreateListing from "./pages/CreateListing";
+import CreateBite from "./pages/CreateBite";
+import styled from "styled-components";
+
+const ViewContainer = styled.div`
+  margin-top: 6.5em;
+`;
 
 //if not logged in, route to pages/LogIn
 const App = () => {
-	return <Router>
+  return (
+    <Router>
       <div>
         <Navbar />
-        <Route exact path="/" component={Landing} />
-        <Route exact path="/home" component={Landing} />
-        <Route exact path="/login" component={LogIn} />
-        <Route exact path="/browse" component={Browse} />
-        <Route exact path="/create" component={CreateListing} />
+        <ViewContainer>
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/home" component={Landing} />
+          <Route exact path="/login" component={LogIn} />
+          <Route exact path="/browse" component={Browse} />
+          <Route exact path="/create" component={CreateBite} />
+        </ViewContainer>
       </div>
-    </Router>;
+    </Router>
+  );
 };
 
 export default App;
--- client/src/components/Search/SearchIcon.js
+++ client/src/components/Search/SearchIcon.js
@@ -2,17 +2,17 @@ import React from "react";
 import styled from "styled-components";
 
 const MySearchIcon = styled.i`
-	color: tomato;
-	float: right;
-	margin-top: 1em;
-	&:hover {
-		cursor: pointer;
-	}
+  color: tomato;
+  float: right;
+  margin-top: 1em;
+  &:hover {
+    cursor: pointer;
+  }
 `;
 
 export const SearchIcon = props => (
-	<MySearchIcon
-		className={props.icon ? "fa fa-search-minus" : "fa fa-search"}
-		onClick={props.onClick}
-	/>
+  <MySearchIcon
+    className={props.icon ? "fa fa-minus-circle" : "fa fa-search"}
+    onClick={props.onClick}
+  />
 );
