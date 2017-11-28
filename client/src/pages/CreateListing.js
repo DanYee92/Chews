8c2092c8cd3083da307024eeaf899caa2985916f
unknown <aliarfeen911@gmail.com>
2017-11-28 13:54:03 -0600

    create bite route working

--- client/src/pages/CreateListing.js
+++ client/src/pages/CreateListing.js
@@ -1,31 +1,100 @@
-import React, {Component} from "react";
-import {ControlLabel} from "react-bootstrap";
+import React, { Component } from "react";
+import { ControlLabel } from "react-bootstrap";
 import { FormGroup, FormInput } from "../components/Form";
 import Button from "../components/Button";
 import Container from "../components/Container";
+import API from "../util/API.js";
 
 class CreateListing extends Component {
-    state = { location: "", startDate: "", endDate: "" };
+	state = { city: "", restaurant: "", startDateRange: "", endDateRange: "" };
 
-    handleInputChange = event => {
-      const { name, value } = event.target;
-      this.setState({ [name]: value });
+	handleInputChange = event => {
+		const { name, value } = event.target;
+		this.setState({ [name]: value });
+	};
+
+	handleFormSubmit = event => {
+		// // for dummy data
+		// const ali = {
+		// 	firstName: "Ali",
+		// 	lastName: "Arfeen",
+		// 	hometown: "Oak Brook"
+		// };
+		// console.log("making ali:", ali);
+		// console.log("calling createNewUser");
+		// API.createNewUser(ali);
+		// const andrew = {
+		// 	firstName: "Andrew",
+		// 	lastName: "Huang",
+		// 	hometown: "Chicago"
+		// };
+		// console.log("making andrew:", andrew);
+		// console.log("calling createNewUser");
+		// API.createNewUser(andrew);
+
+		event.preventDefault();
+
+		/** localId needs to be replaced
+		    with a dynamic Id based on who's
+		    logged in */
+		const localId = "5a1db997fe0a90352c814946";
+
+		const newBite = {
+			localId,
+			city: this.state.city,
+			restaurant: this.state.restaurant,
+			startDateRange: this.state.startDateRange,
+			endDateRange: this.state.endDateRange
 		};
-		render() {
-			return (
-				<Container column>
-					<form>
-						<FormGroup controlId="createListingForm">
-							<ControlLabel>Where do you want to grab a Bite?</ControlLabel>
-							<FormInput type="text" name="email" placeholder="" onChange={this.handleInputChange} />
-							<ControlLabel>When do you want to grab a Bite?</ControlLabel>
-							<FormInput type="text" name="password" placeholder="" onChange={this.handleInputChange} />
-							<Button primary> Create Listing </Button>
-						</FormGroup>
-					</form>
-				</Container>
-			)
-		}
+
+		console.log("localId", localId, "newBite", newBite);
+
+		API.createNewBite(newBite)
+			.then(res => console.log("res", res))
+			.catch(err => console.error(err));
+	};
+
+	render() {
+		return (
+			<Container column>
+				<form onSubmit={this.handleFormSubmit}>
+					<FormGroup controlId="createListingForm">
+						<ControlLabel>
+							Where do you want to grab a Bite?
+						</ControlLabel>
+						<FormInput
+							type="text"
+							name="city"
+							placeholder="City"
+							onChange={this.handleInputChange}
+						/>
+						<FormInput
+							type="text"
+							name="restaurant"
+							placeholder="Restaurant"
+							onChange={this.handleInputChange}
+						/>
+						<ControlLabel>
+							When do you want to grab a Bite?
+						</ControlLabel>
+						<FormInput
+							type="text"
+							name="startDateRange"
+							placeholder="Start Date Range"
+							onChange={this.handleInputChange}
+						/>
+						<FormInput
+							type="text"
+							name="endDateRange"
+							placeholder="End Date Range"
+							onChange={this.handleInputChange}
+						/>
+						<Button primary> Create Listing </Button>
+					</FormGroup>
+				</form>
+			</Container>
+		);
+	}
 }
 
 export default CreateListing;
