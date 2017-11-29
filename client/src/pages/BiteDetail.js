47a7ccb6fcca511bc172a43a4118df66d37b159e
Andrew Huang <andrw.huang@gmail.com>
2017-11-28 22:35:54 -0600

    broken modal on BiteDetail

--- client/src/pages/BiteDetail.js
+++ client/src/pages/BiteDetail.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { Component } from "react";
 import { Grid, Row, Col } from "react-bootstrap";
 import Button from "../components/Button"
 import styled from "styled-components";
+import { OutlineModal } from "boron";
 
 const DetailContainer = styled.div`
 	overflow: hidden;
@@ -14,15 +15,34 @@ const FullWidthImg = styled.img`
 const Divider = styled.hr``;
 const Spacer = styled.br``;
 
-const BiteDetail = () => {
-    return <div>
+class BiteDetail extends Component {
+	showModal() {
+		this.refs.modal.show();
+	}
+
+	hideModal() {
+		this.refs.modal.hide();
+	}
+
+	callback(event){
+        console.log(event);
+  }
+	
+	render() {
+		return <div>
+        <OutlineModal ref="modal" keyboard={this.callback}>
+          <h2>I am a dialog</h2>
+          <button onClick={this.hideModal}>Close</button>
+        </OutlineModal>
         <FullWidthImg src="http://via.placeholder.com/1000x300" />
         <Grid>
           <DetailContainer>
             <Row>
               <Col xs={12} md={4}>
                 <Spacer />
-                <Button primary>Request to Book</Button>
+                <Button primary onClick={this.showModal}>
+                  Request to Book
+                </Button>
                 <Spacer />
                 <Spacer />
                 Grab a Bite with Nicole Ersing
@@ -44,6 +64,8 @@ const BiteDetail = () => {
           </DetailContainer>
         </Grid>
       </div>;
+
+	}	
 }
 
 export default BiteDetail;
