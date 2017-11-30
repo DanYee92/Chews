8f7c419f7da593c725772c92262e90e06dfc511a
Nicole Ersing <nicolegiacci@gmail.com>
2017-11-30 13:20:15 -0600

    auth0 working?

--- client/src/Auth/Auth.js
+++ client/src/Auth/Auth.js
@@ -4,7 +4,7 @@ export default class Auth {
     auth0 = new auth0.WebAuth({
       domain: 'app81460790.auth0.com',
       clientID: 'Evy4W2oGK1HUFAr7XvVAcKTCq-GcF5kP',
-      redirectUri: 'http://localhost:3000/home' ,
+      redirectUri: 'http://chewsapp.herokuapp.com/home' ,
       audience: 'https://app81460790.auth0.com/userinfo',
       responseType: 'token id_token',
       scope: 'openid'
