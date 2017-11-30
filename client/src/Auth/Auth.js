707e122fe4443b70e475ae8686e45bdb646d9504
itkazmi814 <itkazmi814@gmail.com>
2017-11-30 16:04:22 -0600

    merge

--- client/src/Auth/Auth.js
+++ client/src/Auth/Auth.js
@@ -1,7 +1,6 @@
 import auth0 from "auth0-js";
 
 export default class Auth {
-<<<<<<< HEAD
   auth0 = new auth0.WebAuth({
     domain: "app81460790.auth0.com",
     clientID: "Evy4W2oGK1HUFAr7XvVAcKTCq-GcF5kP",
@@ -10,23 +9,6 @@ export default class Auth {
     responseType: "token id_token",
     scope: "openid"
   });
-=======
-    auth0 = new auth0.WebAuth({
-      domain: 'app81460790.auth0.com',
-      clientID: 'Evy4W2oGK1HUFAr7XvVAcKTCq-GcF5kP',
-      redirectUri: 'http://chewsapp.herokuapp.com/home' ,
-      audience: 'https://app81460790.auth0.com/userinfo',
-      responseType: 'token id_token',
-      scope: 'openid'
-    });
-  
-  
-    login() {
-        console.log("working?")
-        console.log(this)
-      this.auth0.authorize();
-    }
->>>>>>> 556510c2af5bdef4fa862dfac99b648494403930
 
   login() {
     console.log("working?");
