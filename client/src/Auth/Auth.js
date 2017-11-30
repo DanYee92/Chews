9e07a93ebf7fea5c4b26ae5574911460e57d55b4
Nicole Ersing <nicolegiacci@gmail.com>
2017-11-30 13:31:26 -0600

    google auth0

--- client/src/Auth/Auth.js
+++ client/src/Auth/Auth.js
@@ -5,7 +5,7 @@ export default class Auth {
       domain: 'app81460790.auth0.com',
       clientID: 'Evy4W2oGK1HUFAr7XvVAcKTCq-GcF5kP',
       redirectUri: 'http://chewsapp.herokuapp.com/home' ,
-      audience: 'https://app81460790.auth0.com/userinfo',
+      audience: 'https://google.com',
       responseType: 'token id_token',
       scope: 'openid'
     });
