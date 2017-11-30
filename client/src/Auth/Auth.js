f07d64a3062d7025f7f3f351f7954dbfc089d5cd
Nicole Ersing <nicolegiacci@gmail.com>
2017-11-30 13:16:29 -0600

    auth0 connected to heroku

--- client/src/Auth/Auth.js
+++ client/src/Auth/Auth.js
@@ -2,11 +2,10 @@ import auth0 from 'auth0-js';
 
 export default class Auth {
     auth0 = new auth0.WebAuth({
-      domain: 'chews.auth0.com',
-      clientID: 'sAwLM4bd82g1nk2d408MeDQxXZfrc63D',
-      
+      domain: 'app81460790.auth0.com',
+      clientID: 'Evy4W2oGK1HUFAr7XvVAcKTCq-GcF5kP',
       redirectUri: 'http://localhost:3000/home' ,
-      audience: 'https://chews.auth0.com/userinfo',
+      audience: 'https://app81460790.auth0.com/userinfo',
       responseType: 'token id_token',
       scope: 'openid'
     });
