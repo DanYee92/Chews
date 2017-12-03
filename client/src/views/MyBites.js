5fbf21e03b928e35c322c99426e3b16dcbb6e528
unknown <aliarfeen911@gmail.com>
2017-12-03 13:25:53 -0600

    my bites wip

--- client/src/views/MyBites.js
+++ client/src/views/MyBites.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Grid, Row, Col, Image } from "react-bootstrap";
+import API from "../util/API";
 import styled from "styled-components";
 
 // Card takes in props: title, local, startDate, endDate, location
@@ -16,42 +17,42 @@ import styled from "styled-components";
 // `;
 
 const CardContainer = styled.div`
-	background: papayawhip;
-	margin-bottom: 2.5em;
+background: papayawhip;
+margin-bottom: 2.5em;
 `;
 
 const CardBody = styled.div`
-	color: black;
-	position: relative;
+color: black;
+position: relative;
 `;
 
 const BiteDate = styled.div`
-	display: inline block;
-	border: tomato 0.1em solid;
-	padding: 0em 0.5em 0em 0.5em;
-	margin-bottom: 0em;
-	background: white;
-	position: absolute;
-	text-align: center;
-	top: 0.5em;
-	right: 1.5em;
-	clear: both;
+display: inline block;
+border: tomato 0.1em solid;
+padding: 0em 0.5em 0em 0.5em;
+margin-bottom: 0em;
+background: white;
+position: absolute;
+text-align: center;
+top: 0.5em;
+right: 1.5em;
+clear: both;
 `;
 
 const BiteMonth = styled.p`
-	font-size: 10px;
-	margin-bottom: 0em;
-	border-bottom: tomato 0.1em solid;
+font-size: 10px;
+margin-bottom: 0em;
+border-bottom: tomato 0.1em solid;
 `;
 
 const BiteDay = styled.p`
-	font-size: 15px;
-	margin-bottom: 0em;
+font-size: 15px;
+margin-bottom: 0em;
 `;
 
 const BiteInfo = styled.h5`
-	width: 100%;
-	float: left;
+width: 100%;
+float: left;
 `;
 
 const noPadding = {
@@ -63,53 +64,63 @@ const noMarginRight = {
 	marginRight: "0em"
 };
 
-export const MyBites = props => {
-	return (
-		<Grid>
-			{props.results ? props.results.map((bite, i) => {
-					return (<CardContainer key={i}>
-						<Row className="show-grid">
-							<Col style={noMarginRight} xs={4} md={3}>
-								<Image src="http://via.placeholder.com/300x200" responsive />
-							</Col>
-							<Col style={noPadding} xs={8} md={9}>
-								<CardBody>
-									<BiteInfo>
-									<p>{bite.restaurant} with {bite.firstName} {bite.lastName}</p>
-									<p>{bite.city}</p>
-									</BiteInfo>
-									{/* insert bite date formatting here */}
-									<BiteDate>
-									<BiteMonth> NOV </BiteMonth>
-									<BiteDay> 14 </BiteDay>
-									</BiteDate>
-								</CardBody>
-							</Col>
-						</Row>
-					</CardContainer>)
-				}) : <CardContainer>
-					<Row className="show-grid">
-						<Col style={noMarginRight} xs={4} md={3}>
-							<Image
-								src="http://via.placeholder.com/300x200"
-								responsive
-							/>
-						</Col>
-						<Col style={noPadding} xs={8} md={9}>
-							<CardBody>
-								<BiteInfo>
-									<p>Jim's Original with Andrew Huang</p>
-									<p>Chicago</p>
-								</BiteInfo>
-								<BiteDate>
-									<BiteMonth> NOV </BiteMonth>
-									<BiteDay> 14 </BiteDay>
-								</BiteDate>
-							</CardBody>
-						</Col>
-					</Row>
-				</CardContainer>
-			}
-		</Grid>
-	);
+export class MyBites extends React.Component {
+	state = {
+		results: null
+	}
+
+	componentDidMount() {
+		console.log("my-bites componentDidMount")
+		console.log(this.props.userId)
+		API.getUserInfo(this.props.userId)
+	}
+
+	render() {
+	// return this.props.userId ? 
+	return <Grid>
+	{this.state.results ? this.state.results.map((bite, i) => {
+		return <CardContainer key={i}>
+              <Row className="show-grid">
+                <Col style={noMarginRight} xs={4} md={3}>
+                  <Image src="http://via.placeholder.com/300x200" responsive />
+                </Col>
+                <Col style={noPadding} xs={8} md={9}>
+                  <CardBody>
+                    <BiteInfo>
+                      <p>
+                        {bite.restaurant} with {bite.firstName} {bite.lastName}
+                      </p>
+                      <p>{bite.city}</p>
+                    </BiteInfo>
+                    {/* insert bite date formatting here */}
+                    <BiteDate>
+                      <BiteMonth> NOV </BiteMonth>
+                      <BiteDay> 14 </BiteDay>
+                    </BiteDate>
+                  </CardBody>
+                </Col>
+              </Row>
+            </CardContainer>;
+        }) : <CardContainer>
+          <Row className="show-grid">
+            <Col style={noMarginRight} xs={4} md={3}>
+              <Image src="http://via.placeholder.com/300x200" responsive />
+            </Col>
+            <Col style={noPadding} xs={8} md={9}>
+              <CardBody>
+                <BiteInfo>
+                  <p>Jim's Original with Andrew Huang</p>
+                  <p>Chicago</p>
+                </BiteInfo>
+                <BiteDate>
+                  <BiteMonth> NOV </BiteMonth>
+                  <BiteDay> 14 </BiteDay>
+                </BiteDate>
+              </CardBody>
+            </Col>
+          </Row>
+        </CardContainer>}
+	</Grid>
+	// : <div>{this.props.history.replace("/")}</div>
+	}
 };
